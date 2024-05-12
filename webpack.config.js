20c54ae3b15001726846f9a75234ba9dea54e05a
d.zhiborkin <danilkaololo67@gmail.com>
2024-05-12 21:10:46 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
